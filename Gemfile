e90fb8f7012ee2af43a894e390bb9b881169d818
hg16 <hmgaar@gmail.com>
2016-02-04 20:29:10 -0500

    added quotes to seeds.rb and pages.js -- next to work Quote Model CRUD

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'https://rubygems.org'
 
+#NOTE Gems added
 
+group :production do
+  gem 'rails_12factor'
+end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,297 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+quotes = [
+  {
+  title: "For this to work, we have to abandon traditional thinking from top to bottom, & left to right. Do everything differently #startup",
+  author: ''
+  },
+  {
+  title: "I want to be the example of who my dauthers look for in future boyfriend",
+  author: ''
+  },
+  {
+  title: 'This commitment, this sacrifice and these risks that I am taking - I am only able to do that on the strength of I’m doing it for our family. This is not about press for me but the legacy I am trying to leave for my family.',
+  author: ''
+  },
+  {
+  title: "Entrepreneurial life is a roller coaster and the Lewis family has navigated that journey by staying connected. Communication is the key.",
+  author: ''
+  },
+  {
+  title: "For this to work, we have to abandon traditional thinking from top to bottom, & left to right. Do everything differently #startup",
+  author: ''
+  },
+  {
+  title: "Purpose cant be quantified with metrics, it can only be realized w/ the understanding of multiplying your dreams and efforts.",
+  author: ''
+  },
+  {
+  title: "Your success depends on its second letter.",
+  author: ''
+  },
+  {
+  title: "Don't wish it were easier, wish you were better.",
+  author: ''
+  },
+  {
+  title: "Enthusiasm is infectious & energizing. People are motivated by leaders who possess a vision for an amazing today and a better tomorrow.",
+  author: ''
+  },
+  {
+  title: "Theres a distinct difference from being unemployed and entrepreneurship.",
+  author: ''
+  },
+  {
+  title: "We can succeed at almost anything if we have unlimited ambition, passion, enthusiasm and an unyielding dedication! #capacity",
+  author: ''
+  },
+  {
+  title: "Get really good and consistent at the basics.",
+  author: ''
+  },
+  {
+  title: "The start has startled a lot of people. Dont be afraid of being a beginner.",
+  author: ''
+  },
+  {
+  title: "Dreaming of winning doesnt make you a winner, winning makes you a winner.",
+  author: ''
+  },
+  {
+  title: "Most leaders you can see and hear but the unique ones, the special ones you can feel.",
+  author: ''
+  },
+  {
+  title: "2 day is a new day 0 time to waste 1 grand vision 6 times the success 2016 is a 365 step program to turn your dreams into reality.",
+  author: ''
+  },
+  {
+  title: 'Its stories like these that help keep me and the team motivated. Its these stories that reinforce, "yeah... Payroll should be free." Weve been in the lab non stop 15-20 hour days, weekends, all-nighters, disagreements, breakthroughs, highs and lows, setbacks, missed timelines, relationship building, sacrifices and we are still busting our butts and have lots of work to do. But as a team we know its worth it.',
+  author: ''
+  },
+  {
+  title: "Making a difference is the best way to make a dollar. Funny how that works.",
+  author: ''
+  },
+  {
+  title: "In a world thats built on Instant Gratification and so overly focused on time management, time constraints, appointments, schedules, time tables, perfect timing, it hard to create something timeless, but its worth your time to do so.",
+  author: ''
+  },
+  {
+  title: "People are fascinated with how power works and how deals get done. The most intense stories of power and greatness are usually based on someones epic desire and singular focus to accomplish a particular thing.",
+  author: ''
+  },
+  {
+  title: "So many people are upset with their 9 to 5 but they are sleep right now. Theres something special about doing what you love and becoming who you are meant to be. Greatness will require you to demand more of yourself. Ask yourself what are you committed too? What are you risking? #WhatsPossibleWednesdays",
+  author: ''
+  },
+  {
+  title: "Excuses like opportunities will always make themselves available to you.",
+  author: ''
+  },
+  {
+  title: "You never notice the time is right then do it, you always just do it and then realize the time was right.",
+  author: ''
+  },
+  {
+  title: "Create & ignite energy and be able to put capital to work.",
+  author: ''
+  },
+  {
+  title: "There's a difference between starting a #startup and having a #sidebusiness",
+  author: ''
+  },
+  {
+  title: "Until youre ready to risk it all., youre not ready to have it all.",
+  author: ''
+  },
+  {
+  title: "Lots of gr8 #startups started 5-8 yrs ago. What new great companies are being built now? Hmmm I know 1.",
+  author: ''
+  },
+  {
+  title: "Well designed #pitchdeck -> well designed products -> well designed #startup #designthinking",
+  author: ''
+  },
+  {
+  title: "the bigger the checks the less time investors spend on the decks. 3mins top. Short, beautiful & concise. #startup #pitchdeck",
+  author: ''
+  },
+  {
+  title: "2 rules of #success 1. Never quit 2. Repeat",
+  author: ''
+  },
+  {
+  title: "There should be no need for a casual Friday if you work at a tech startup #Wearwhatyouwantatworkeveryday",
+  author: ''
+  },
+  {
+  title: "Before a business can be built, a dream must be dreamed.",
+  author: ''
+  },
+  {
+  title: "Believe in limitless possibilities and opportunities.",
+  author: ''
+  },
+  {
+  title: "You cant build an A team, with B players. #startups",
+  author: ''
+  },
+  {
+  title: "Investment tip: invest as much as you can in this… YOURSELF.",
+  author: ''
+  },
+  {
+  title: "When solving a really big problem its just as much about finding the right people as it is about finding product marketing fit.",
+  author: ''
+  },
+  {
+  title: "Better to not have a dime than to not have a dream.",
+  author: ''
+  },
+  {
+  title: "Those that create the future, make history.",
+  author: ''
+  },
+  {
+  title: "I do not wake up everyday to be a witness. Success isnt a spectator sport.",
+  author: ''
+  },
+  {
+  title: "Building a great business transcends everything.",
+  author: ''
+  },
+  {
+  title: "Them: Craig what do you actually do? Me: Whatever it takes. #entrepreneur",
+  author: ''
+  },
+  {
+  title: "Whats on the other side of fear? Nothing.",
+  author: ''
+  },
+  {
+  title: "If you are focused on building a gr8 business youre not as nervous as those trying to build #unicorns #substance #Startups",
+  author: ''
+  },
+  {
+  title: ,
+  author: ''
+  },
+  {
+  title: ,
+  author: ''
+  },
+  {
+  title: 'Everyday is a "weekend". You can also plug in "holiday" or "vacation" or "Friday" whatever we like we should embody that everyday.',
+  author: ''
+  },
+  {
+  title: "Your actions are your equity. If you take no action you have no value.",
+  author: ''
+  },
+  {
+  title: "There is no rule book on how 2change the world. Theres no selection process on who gets 2change it. U just decide & do it.",
+  author: ''
+  },
+  {
+  title: "We arent concerned w/ designing a gr8 vaca package 4our team were more focused on building a company where they dont want a vaca #culture",
+  author: ''
+  },
+  {
+  title: "I propose that execution is not everything. #Entrepreneurship",
+  author: ''
+  },
+  {
+  title: "I want to share free payroll with the world.",
+  author: ''
+  },
+  {
+  title: "Nothing scales like #code The power of startups is that small teams have the power 2build multi-billion $ businesses w/ just lines of code.",
+  author: ''
+  },
+  {
+  title: "u start a #startup & then u transition into building a business. The sooner the better. u can keep the core #culture as you grow.",
+  author: ''
+  },
+  {
+  title: "Everyone has an opinion, dont be distracted from your vision.",
+  author: ''
+  },
+  {
+  title: "Money will chase opportunity.",
+  author: ''
+  },
+  {
+  title: "Strategy: Take risk. Keep showing up when most would quit.",
+  author: ''
+  },
+  {
+  title: "Impossible is just impotent inspiration.",
+  author: ''
+  },
+  {
+  title: "Why not build a biz thats orders of magnitude better than any1 else 4every1 involved. If u arent going 2 build one, join one and go hard.",
+  author: ''
+  },
+  {
+  title: "Being 0 #Revenue BY CHOICE lets you focus solely on hyper-fast customer growth & rely on the financial markets4, I couldnt imagine building a startup w/out a designer itd be like so pointless!",
+  author: ''
+  },
+  {
+  title: "The amount of time u have in this life to leave your legacy is limited. That should be motivation enough. Man I Love Mondays.",
+  author: ''
+  },
+  {
+  title: '"Can I build a tech startup without a technical cofounder?" - Yes. "Can I build a real business without a business cofounder?" - Of course.',
+  author: ''
+  },
+  {
+  title: "Design each and every day around your dreams. Dream->Decide->Do->Dedicate->Deliver",
+  author: ''
+  },
+  {
+  title: "Imagine the things you could do if you just did the things you imagine.",
+  author: ''
+  },
+  {
+  title: "To distance yourself from your problems, dedicate yourself to your purpose.",
+  author: ''
+  },
+  {
+  title: "Customer Success is not a position title its a way of life.",
+  author: ''
+  },
+  {
+  title: "Your work ethic is where youll discover your WOW factor.",
+  author: ''
+  },
+  {
+  title: "75% of today was problem solving. Understand the barrier, ask the right questions, recognize the opportunity and empower people to execute.",
+  author: ''
+  },
+]
+
+=begin
+"We cant take responsibility for our actions only after were caught; we take responsibility at the time we act -US Marine"
+
+'"I do not believe a man can ever leave his business. He ought to think of it by day and dream of it by night." -Henry Ford'
+'"Meaning either you do or you dont, but theres no penalty, theres no reward." Jamie Foxx'
+
+'"The last 10% it takes to launch something takes as much energy as the first 90%." –Rob Kalin, co-founder of Etsy'
+
+"Make something people want includes making a company that people want to work for. –Sahil Lavingia, founder of Gumroad."
+
+"Wake up everyday trying to change the world, not trying to get acquired - Brian Brackeen"
+"I don’t pay good wages because I have a lot of money; I have a lot of money because I pay good wages. - Robert Bosch"
+"Don’t market stuff, market movements. - Jules Marcoux"
+
+# TODO NOT TO ADD CAUSE THERE FROMSOMEONE ELSE
+=end
+
+
+
+
 
 other_quotes = [
   {
@@ -73,152 +364,3 @@ other_quotes = [
     author: "Albert Einstein"
   }
 ];
-
-
-
-
-"For this to work, we have to abandon traditional thinking from top to bottom, & left to right. Do everything differently #startup"
-
-"I want to be the example of who my dauthers look for in future boyfriend"
-
-"This commitment, this sacrifice and these risks that I am taking - I am only able to do that on the strength of I’m doing it for our family.” This is not about press for me but the legacy I am trying to leave for my family."
-
-"Entrepreneurial life is a roller coaster and the Lewis' family has navigated that journey by staying connected. Communication is the key."
-
-"For this to work, we have to abandon traditional thinking from top to bottom, & left to right. Do everything differently #startup"
-
-"Purpose cant be quantified with metrics, it can only be realized w/ the understanding of multiplying your dreams and efforts."
-
-"Your success depends on its second letter."
-
-"Don't wish it were easier, wish you were better."
-
-"Enthusiasm is infectious & energizing. People are motivated by leaders who possess a vision for an amazing today and a better tomorrow."
-
-"Theres a distinct difference from being unemployed and entrepreneurship."
-
-"We can succeed at almost anything if we have unlimited ambition, passion, enthusiasm and an unyielding dedication! #capacity"
-
-"Get really good and consistent at the basics."
-
-"The start has startled a lot of people. Dont be afraid of being a beginner."
-
-"Dreaming of winning doesnt make you a winner, winning makes you a winner."
-
-"Most leaders you can see and hear but the unique ones, the special ones you can feel."
-
-"We cant take responsibility for our actions only after were caught; we take responsibility at the time we act -US Marine"
-
-'"I do not believe a man can ever leave his business. He ought to think of it by day and dream of it by night." -Henry Ford'
-
-"2 day is a new day 0 time to waste 1 grand vision 6 times the success 2016 is a 365 step program to turn your dreams into reality."
-
-'Its stories like these that help keep me and the team motivated. Its these stories that reinforce, "yeah... Payroll should be free." Weve been in the lab non stop 15-20 hour days, weekends, all-nighters, disagreements, breakthroughs, highs and lows, setbacks, missed timelines, relationship building, sacrifices and we are still busting our butts and have lots of work to do. But as a team we know its worth it.'
-
-"Making a difference is the best way to make a dollar. Funny how that works."
-
-"In a world thats built on Instant Gratification and so overly focused on time management, time constraints, appointments, schedules, time tables, perfect timing, it hard to create something timeless, but its worth your time to do so."
-
-"People are fascinated with how power works and how deals get done. The most intense stories of power and greatness are usually based on someones epic desire and singular focus to accomplish a particular thing."
-
-"So many people are upset with their 9 to 5 but they are sleep right now. Theres something special about doing what you love and becoming who you are meant to be. Greatness will require you to demand more of yourself. Ask yourself what are you committed too? What are you risking? #WhatsPossibleWednesdays"
-
-"Excuses like opportunities will always make themselves available to you."
-
-"You never notice the time is right then do it, you always just do it and then realize the time was right."
-
-"Create & ignite energy and be able to put capital to work."
-
-"There's a difference between starting a #startup and having a #sidebusiness"
-
-"Until youre ready to risk it all., youre not ready to have it all."
-
-"Lots of gr8 #startups started 5-8 yrs ago. What new great companies are being built now? Hmmm I know 1."
-
-"Well designed #pitchdeck -> well designed products -> well designed #startup #designthinking"
-
-"the bigger the checks the less time investors spend on the decks. 3mins top. Short, beautiful & concise. #startup #pitchdeck"
-
-"2 rules of #success 1. Never quit 2. Repeat"
-
-"Whats on the other side of fear? Nothing."
-
-'"Meaning either you do or you dont, but theres no penalty, theres no reward." Jamie Foxx'
-
-"There should be no need for a casual Friday if you work at a tech startup Wearwhatyouwantatworkeveryday"
-
-"Before a business can be built, a dream must be dreamed."
-
-"Believe in limitless possibilities and opportunities."
-
-"Them: Craig what do you actually do? Me: Whatever it takes. #entrepreneur"
-
-"Building a great business transcends everything."
-
-"You cant build an A team, with B players. #startups"
-
-"Investment tip: invest as much as you can in this… YOURSELF."
-
-"When solving a really big problem its just as much about finding the right people as it is about finding product marketing fit."
-
-'"The last 10% it takes to launch something takes as much energy as the first 90%." –Rob Kalin, co-founder of Etsy'
-
-"Make something people want includes making a company that people want to work for. –Sahil Lavingia, founder of Gumroad."
-
-"Better to not have a dime than to not have a dream."
-
-"Those that create the future, make history."
-
-"Why not build a biz thats orders of magnitude better than any1 else 4every1 involved. If u arent going 2 build one, join one and go hard."
-
-"I do not wake up everyday to be a witness. Success isnt a spectator sport."
-
-"u start a #startup & then u transition into building a business. The sooner the better. u can keep the core #culture as you grow."
-
-"Impossible is just impotent inspiration."
-
-"Strategy: Take risk. Keep showing up when most would quit."
-
-"Nothing scales like #code The power of startups is that small teams have the power 2build multi-billion $ businesses w/ just lines of code."
-
-"Money will chase opportunity."
-
-"Everyone has an opinion, dont be distracted from your vision."
-
-"We arent concerned w/ designing a gr8 vaca package 4our team were more focused on building a company where they dont want a vaca #culture"
-
-"I propose that execution is not everything. #Entrepreneurship"
-
-"There is no rule book on how 2change the world. Theres no selection process on who gets 2change it. U just decide&do it."
-
-"Your actions are your equity. If you take no action you have no value."
-
-'Everyday is a "weekend". You can also plug in "holiday" or "vacation" or "Friday" whatever we like we should embody that everyday.'
-
-"If you are focused on building a gr8 business youre not as nervous as those trying to build #unicorns #substance #Startups"
-
-"75% of today was problem solving. Understand the barrier, ask the right questions, recognize the opportunity and empower people to execute."
-
-"I want to share free payroll with the world."
-
-"Your work ethic is where youll discover your WOW factor."
-
-"Customer Success is not a position title its a way of life."
-
-"Wake up everyday trying to change the world, not trying to get acquired - Brian Brackeen"
-
-"To distance yourself from your problems, dedicate yourself to your purpose."
-
-"The amount of time u have in this life to leave your legacy is limited. That should be motivation enough. Man I Love Mondays."
-
-"Imagine the things you could do if you just did the things you imagine."
-
-"Being 0 #Revenue BY CHOICE lets you focus solely on hyper-fast customer growth & rely on the financial markets4, I couldnt imagine building a startup w/out a designer itd be like so pointless!"
-
-"I don’t pay good wages because I have a lot of money; I have a lot of money because I pay good wages. - Robert Bosch"
-
-"Design each and every day around your dreams. Dream->Decide->Do->Dedicate->Deliver"
-
-'"Can I build a tech startup without a technical cofounder?" - Yes. "Can I build a real business without a business cofounder?" - Of course.'
-
-"Don’t market stuff, market movements. - Jules Marcoux"
