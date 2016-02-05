59854539fb4edc5a91c863b62de789dc311473e7
hg16 <hmgaar@gmail.com>
2016-02-04 19:53:30 -0500

    change and added Quote Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,220 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+other_quotes = [
+  {
+    title: "Design is not just what it looks like and feels like. Design is how it works.",
+    author: "Steve Jobs"
+  },
+  {
+    title: "Nothing is impossible, the word itself says “I’m possible”!",
+    author: "Audrey Hepburn"
+  },
+  {
+    title: "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+    author: "Maya Angelou"
+  },
+  {
+    title: "Whether you think you can or you think you can’t, you’re right.",
+    author: "Henry Ford"
+  },
+  {
+    title: "Perfection is not attainable, but if we chase perfection we can catch excellence.",
+    author: "Vince Lombardi"
+  },
+  {
+    title: "Life is 10% what happens to me and 90% of how I react to it.",
+    author: "Charles Swindoll"
+  },
+  {
+    title: "If you look at what you have in life, you’ll always have more. If you look at what you don’t have in life, you’ll never have enough",
+    author: "Oprah Winfrey"
+  },
+  {
+    title: "Remember no one can make you feel inferior without your consent.",
+    author: "Eleanor Roosevelt"
+  },
+  {
+    title: "I can’t change the direction of the wind, but I can adjust my sails to always reach my destination.",
+    author: "Jimmy Dean"
+  },
+  {
+    title: "Believe you can and you’re halfway there.",
+    author: "Theodore Roosevelt"
+  },
+  {
+    title: "To handle yourself, use your head; to handle others, use your heart.",
+    author: "Eleanor Roosevelt"
+  },
+  {
+    title: "Too many of us are not living our dreams because we are living our fears.",
+    author: "Les Brown"
+  },
+  {
+    title: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein"
+  },
+  {
+    title: "It is during our darkest moments that we must focus to see the light.",
+    author: "Aristotle Onassis"
+  },
+  {
+    title: "Change your thoughts and you change your world.",
+    author: "Norman Vincent Peale"
+  },
+  {
+    title: "A person who never made a mistake never tried anything new.",
+    author: "Albert Einstein"
+  }
+];
+
+
+
+
+"For this to work, we have to abandon traditional thinking from top to bottom, & left to right. Do everything differently #startup"
+
+"I want to be the example of who my dauthers look for in future boyfriend"
+
+"This commitment, this sacrifice and these risks that I am taking - I am only able to do that on the strength of I’m doing it for our family.” This is not about press for me but the legacy I am trying to leave for my family."
+
+"Entrepreneurial life is a roller coaster and the Lewis' family has navigated that journey by staying connected. Communication is the key."
+
+"For this to work, we have to abandon traditional thinking from top to bottom, & left to right. Do everything differently #startup"
+
+"Purpose cant be quantified with metrics, it can only be realized w/ the understanding of multiplying your dreams and efforts."
+
+"Your success depends on its second letter."
+
+"Don't wish it were easier, wish you were better."
+
+"Enthusiasm is infectious & energizing. People are motivated by leaders who possess a vision for an amazing today and a better tomorrow."
+
+"Theres a distinct difference from being unemployed and entrepreneurship."
+
+"We can succeed at almost anything if we have unlimited ambition, passion, enthusiasm and an unyielding dedication! #capacity"
+
+"Get really good and consistent at the basics."
+
+"The start has startled a lot of people. Dont be afraid of being a beginner."
+
+"Dreaming of winning doesnt make you a winner, winning makes you a winner."
+
+"Most leaders you can see and hear but the unique ones, the special ones you can feel."
+
+"We cant take responsibility for our actions only after were caught; we take responsibility at the time we act -US Marine"
+
+'"I do not believe a man can ever leave his business. He ought to think of it by day and dream of it by night." -Henry Ford'
+
+"2 day is a new day 0 time to waste 1 grand vision 6 times the success 2016 is a 365 step program to turn your dreams into reality."
+
+'Its stories like these that help keep me and the team motivated. Its these stories that reinforce, "yeah... Payroll should be free." Weve been in the lab non stop 15-20 hour days, weekends, all-nighters, disagreements, breakthroughs, highs and lows, setbacks, missed timelines, relationship building, sacrifices and we are still busting our butts and have lots of work to do. But as a team we know its worth it.'
+
+"Making a difference is the best way to make a dollar. Funny how that works."
+
+"In a world thats built on Instant Gratification and so overly focused on time management, time constraints, appointments, schedules, time tables, perfect timing, it hard to create something timeless, but its worth your time to do so."
+
+"People are fascinated with how power works and how deals get done. The most intense stories of power and greatness are usually based on someones epic desire and singular focus to accomplish a particular thing."
+
+"So many people are upset with their 9 to 5 but they are sleep right now. Theres something special about doing what you love and becoming who you are meant to be. Greatness will require you to demand more of yourself. Ask yourself what are you committed too? What are you risking? #WhatsPossibleWednesdays"
+
+"Excuses like opportunities will always make themselves available to you."
+
+"You never notice the time is right then do it, you always just do it and then realize the time was right."
+
+"Create & ignite energy and be able to put capital to work."
+
+"There's a difference between starting a #startup and having a #sidebusiness"
+
+"Until youre ready to risk it all., youre not ready to have it all."
+
+"Lots of gr8 #startups started 5-8 yrs ago. What new great companies are being built now? Hmmm I know 1."
+
+"Well designed #pitchdeck -> well designed products -> well designed #startup #designthinking"
+
+"the bigger the checks the less time investors spend on the decks. 3mins top. Short, beautiful & concise. #startup #pitchdeck"
+
+"2 rules of #success 1. Never quit 2. Repeat"
+
+"Whats on the other side of fear? Nothing."
+
+'"Meaning either you do or you dont, but theres no penalty, theres no reward." Jamie Foxx'
+
+"There should be no need for a casual Friday if you work at a tech startup Wearwhatyouwantatworkeveryday"
+
+"Before a business can be built, a dream must be dreamed."
+
+"Believe in limitless possibilities and opportunities."
+
+"Them: Craig what do you actually do? Me: Whatever it takes. #entrepreneur"
+
+"Building a great business transcends everything."
+
+"You cant build an A team, with B players. #startups"
+
+"Investment tip: invest as much as you can in this… YOURSELF."
+
+"When solving a really big problem its just as much about finding the right people as it is about finding product marketing fit."
+
+'"The last 10% it takes to launch something takes as much energy as the first 90%." –Rob Kalin, co-founder of Etsy'
+
+"Make something people want includes making a company that people want to work for. –Sahil Lavingia, founder of Gumroad."
+
+"Better to not have a dime than to not have a dream."
+
+"Those that create the future, make history."
+
+"Why not build a biz thats orders of magnitude better than any1 else 4every1 involved. If u arent going 2 build one, join one and go hard."
+
+"I do not wake up everyday to be a witness. Success isnt a spectator sport."
+
+"u start a #startup & then u transition into building a business. The sooner the better. u can keep the core #culture as you grow."
+
+"Impossible is just impotent inspiration."
+
+"Strategy: Take risk. Keep showing up when most would quit."
+
+"Nothing scales like #code The power of startups is that small teams have the power 2build multi-billion $ businesses w/ just lines of code."
+
+"Money will chase opportunity."
+
+"Everyone has an opinion, dont be distracted from your vision."
+
+"We arent concerned w/ designing a gr8 vaca package 4our team were more focused on building a company where they dont want a vaca #culture"
+
+"I propose that execution is not everything. #Entrepreneurship"
+
+"There is no rule book on how 2change the world. Theres no selection process on who gets 2change it. U just decide&do it."
+
+"Your actions are your equity. If you take no action you have no value."
+
+'Everyday is a "weekend". You can also plug in "holiday" or "vacation" or "Friday" whatever we like we should embody that everyday.'
+
+"If you are focused on building a gr8 business youre not as nervous as those trying to build #unicorns #substance #Startups"
+
+"75% of today was problem solving. Understand the barrier, ask the right questions, recognize the opportunity and empower people to execute."
+
+"I want to share free payroll with the world."
+
+"Your work ethic is where youll discover your WOW factor."
+
+"Customer Success is not a position title its a way of life."
+
+"Wake up everyday trying to change the world, not trying to get acquired - Brian Brackeen"
+
+"To distance yourself from your problems, dedicate yourself to your purpose."
+
+"The amount of time u have in this life to leave your legacy is limited. That should be motivation enough. Man I Love Mondays."
+
+"Imagine the things you could do if you just did the things you imagine."
+
+"Being 0 #Revenue BY CHOICE lets you focus solely on hyper-fast customer growth & rely on the financial markets4, I couldnt imagine building a startup w/out a designer itd be like so pointless!"
+
+"I don’t pay good wages because I have a lot of money; I have a lot of money because I pay good wages. - Robert Bosch"
+
+"Design each and every day around your dreams. Dream->Decide->Do->Dedicate->Deliver"
+
+'"Can I build a tech startup without a technical cofounder?" - Yes. "Can I build a real business without a business cofounder?" - Of course.'
+
+"Don’t market stuff, market movements. - Jules Marcoux"
